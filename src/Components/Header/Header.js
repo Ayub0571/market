ad83c15194bcdd0a5c4647c1b1c7ae0fe0118301
baisal <baisaliulchibaev@gmail.com>
2022-10-02 01:32:56 +0600

    Изменил

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,8 +7,15 @@ const Header = () => {
       <div className="header">
         <div className="header__logo"></div>
         <div className="header__login">
-          <div className="">
-            <input className="header__input" type="text" />
+          <div className="custom-search">
+            <input
+              type="text"
+              className="custom-search-input"
+              placeholder="Найти глубину..."
+            />
+            <button className="custom-search-botton" type="submit">
+              Subscribe
+            </button>
           </div>
           <div className="header__sign-in">Sign in</div>
           <div className="header__sign-up">Sign up</div>
---
+++ src/Components/OceanAnimations/Animation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Animation.scss";
+
+const Animation = () => {
+  return (
+    <div className="ocean">
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+  );
+};
+
+export default Animation;
