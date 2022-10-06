69f2cdbcf0b5866dd30421099d3149f189705cc5
baisal <baisaliulchibaev@gmail.com>
2022-10-06 16:03:24 +0600

    Изменил About

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,24 +1,41 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './Routs.css'
-
-
-
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Routs.css";
+import "./About.scss";
 
 const About = () => {
   return (
     <div>
-      <h1>Blog</h1>
-      <h2>Something about us. Go <Link to='/' className='a'>Home</Link> </h2>
-      <img src='https://www.impactplus.com/hubfs/blog-image-uploads/best-about-us-pages.jpg' alt='About'/>
+      <div className="comands">
+        <div className="Asel">
+          <h1>Asel</h1>
+          <img
+            src="https://www.pngall.com/wp-content/uploads/6/Simpsons-Movie-PNG-Image-File.png"
+            alt=""
+          />
+          <br />
+          <button>Узнать подробнее про меня</button>
+        </div>
+        <div className="Bais">
+          <h1>Baisal</h1>
+          <img
+            src="https://avatanplus.com/files/resources/original/57a749ac7547a1566577c9c0.png"
+            alt=""
+          />
+          <br />
+          <button>Узнать подробнее про меня</button>
+        </div>
+        <div className="Aiub">
+          <h1>Aiub</h1>
+          <img
+            src="https://www.pngall.com/wp-content/uploads/6/Simpsons-Movie-PNG-Image-File.png"
+            alt=""
+          />
+          <br />
+          <button>Узнать подробнее про меня</button>
+        </div>
+      </div>
     </div>
-  )
-}
-export default  About
-  
-
-
-
-
-
+  );
+};
+export default About;
