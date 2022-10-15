2046d9f192042ca63bbf79f7ed49da7e5efae825
Vlad <aubbabamur@gmail.com>
2022-10-15 20:14:52 +0600

    delete components

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import { Layout } from "./Routs/Layout";
 import { Home } from "./Pages/Home.js";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Polar_bear } from "./marine_inhabitants/Polar_bear.js";
-import { Penguin } from "marine_inhabitants/Penguin";
-import { Sea_calf } from "marine_inhabitants/Sea_calf";
-import { Sea_otter } from "marine_inhabitants/Sea ​_otter";
-import { Sea_turtle } from "marine_inhabitants/Sea ​_turtle";
+// import { Polar_bear } from "./marine_inhabitants/Polar_bear.js";
+// import { Penguin } from "marine_inhabitants/Penguin";
+// import { Sea_calf } from "marine_inhabitants/Sea_calf";
+// import { Sea_otter } from "marine_inhabitants/Sea ​_otter";
+// import { Sea_turtle } from "marine_inhabitants/Sea ​_turtle";
 import { Dolphin } from "marine_inhabitants/Dolphin";
-import { Octopus } from "marine_inhabitants/Octopus";
-import { Shark } from "marine_inhabitants/Shark";
-import { Killer_whale } from "marine_inhabitants/Killer_whale";
+// import { Octopus } from "marine_inhabitants/Octopus";
+// import { Shark } from "marine_inhabitants/Shark";
+// import { Killer_whale } from "marine_inhabitants/Killer_whale";
 
 function App() {
   const useScrollToTop = () => {
@@ -33,15 +33,15 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/posts" element={<Blog />} />
-        <Route path="/bear" element={<Polar_bear />} />
-        <Route path="/penguin" element={<Penguin />} />
-        <Route path="/calf" element={<Sea_calf />} />
-        <Route path="/otter" element={<Sea_otter />} />
-        <Route path="/turtle" element={<Sea_turtle />} />
+        {/* <Route path="/bear" element={<Polar_bear />} /> */}
+        {/* <Route path="/penguin" element={<Penguin />} /> */}
+        {/* <Route path="/calf" element={<Sea_calf />} /> */}
+        {/* <Route path="/otter" element={<Sea_otter />} /> */}
+        {/* <Route path="/turtle" element={<Sea_turtle />} /> */}
         <Route path="/dolphin" element={<Dolphin />} />
-        <Route path="/octopus" element={<Octopus />} />
-        <Route path="/shark" element={<Shark />} />
-        <Route path="/killer_whale" element={<Killer_whale />} />
+        {/* <Route path="/octopus" element={<Octopus />} /> */}
+        {/* <Route path="/shark" element={<Shark />} /> */}
+        {/* <Route path="/killer_whale" element={<Killer_whale />} /> */}
         <Route path="/animal" element={<Dolphin />} />
         <Route path="*" element={<Not />} />
       </Route>
--- src/marine_inhabitants/Dolphin.js
+++ src/marine_inhabitants/Dolphin.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "../fishCss/fish.css";
-import "./Dolphin.scss";
 
 const Dolphin = () => {
   const location = useLocation();
 
-  const [data, setData] = useState({ name: "", img: "", description: "" });
+  const [data, setData] = useState({ name: "", img: "", description: "", video: "" });
 
   useEffect(() => {
     const search = {};
@@ -17,32 +16,26 @@ const Dolphin = () => {
         const tmp = el.split("=");
         search[tmp[0]] = tmp[1];
       });
+      
     console.log(search);
-    setData(search);
+    setData (search);
   }, []);
 
   return (
     <div className="dolphin">
       <div>
         <h1>{data.name}</h1>
-        {/* <h1>Dolphin</h1> */}
+        
         <p>Discription</p>
         <img src={data.img} alt="" />
-        {/* <img src="https://www.cmaquarium.org/app/uploads/2021/06/2021.04-Nick-Underwater-0001-768x512.jpg" /> */}
         <p>
-          {/* Дельфи́ны — водные млекопитающие инфраотряда китообразных,
-          принадлежащие либо к семейству дельфиновых (Delphinidae) — морские,
-          либо к нетаксономической группе речных дельфинов — пресноводные. Любят
-          воду и мелкую рыбу. */}
           {data.description}
         </p>
         <div className="ratio ratio-16x9">
-          <iframe
-            src="https://www.youtube.com/embed/G7L4YzGAvMA"
-            title="YouTube video"
-            allowFullScreen
-            className="vidbear"
-          ></iframe>
+        <iframe src= {data.video}
+                      title="YouTube video"
+                      allowFullScreen
+                      className="vidbear"/>
         </div>
       </div>
     </div>
