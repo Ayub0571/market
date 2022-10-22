521a33a5bc3651d29b32b273e79326c3ebde30ff
baisal <baisaliulchibaev@gmail.com>
2022-10-22 22:39:25 +0600

    l

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css'
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Blog } from "./Pages/Blog";
 import About from "./Pages/About";
@@ -8,20 +8,7 @@ import { Layout } from "./Routs/Layout";
 import { Home } from "./Pages/Home.js";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-<<<<<<< HEAD
-import {Animal } from './marine_inhabitants/Animal'
-=======
-import { Polar_bear } from "./marine_inhabitants/Polar_bear.js";
-import { Penguin } from "marine_inhabitants/Penguin";
-import { Sea_calf } from "marine_inhabitants/Sea_calf";
-import { Sea_otter } from "marine_inhabitants/Sea ​_otter";
-import { Sea_turtle } from "marine_inhabitants/Sea ​_turtle";
-import { Dolphin } from "marine_inhabitants/Dolphin";
-import { Octopus } from "marine_inhabitants/Octopus";
-import { Shark } from "marine_inhabitants/Shark";
-import { Killer_whale } from "marine_inhabitants/Killer_whale";
->>>>>>> baisal
-
+import { Animal } from "./marine_inhabitants/Animal";
 function App() {
   const useScrollToTop = () => {
     const { pathname } = useLocation();
@@ -33,35 +20,17 @@ function App() {
   useScrollToTop();
 
   return (
-<<<<<<< HEAD
     <div className="App">
-=======
->>>>>>> baisal
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/posts" element={<Blog />} />
-<<<<<<< HEAD
-        <Route path="/animal" element={<Animal />} />
-        <Route path="*" element={<Not />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/posts" element={<Blog />} />
+          <Route path="/animal" element={<Animal />} />
+          <Route path="*" element={<Not />} />
+        </Route>
+      </Routes>
     </div>
-=======
-        <Route path="*" element={<Not />} />
-        <Route path="/bear" element={<Polar_bear />} />
-        <Route path="/penguin" element={<Penguin />} />
-        <Route path="/calf" element={<Sea_calf />} />
-        <Route path="/otter" element={<Sea_otter />} />
-        <Route path="/turtle" element={<Sea_turtle />} />
-        <Route path="/dolphin" element={<Dolphin />} />
-        <Route path="/octopus" element={<Octopus />} />
-        <Route path="/shark" element={<Shark />} />
-        <Route path="/killer_whale" element={<Killer_whale />} />
-      </Route>
-    </Routes>
->>>>>>> baisal
   );
 }
 
